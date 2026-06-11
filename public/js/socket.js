a077b0f5b28bb923dba1174ca5f7a673cbfab84f
4724061-alt <4724061@gse.okayama-c.ed.jp>
2026-06-11 13:49:18 +0900

    Update socket.js

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,1 +1,12 @@
+const socket = io();
+
+// 自分のユーザールームに入る
+function joinUserRoom(userId) {
+  socket.emit("joinUser", userId);
+}
+
+// コミュニティに入る
+function joinCommunityRoom(communityId) {
+  socket.emit("joinCommunity", communityId);
+}
 
