c63c1cd7a60bb0f00bf51e4b20fb3aca1b599b2b
4724061-alt <4724061@gse.okayama-c.ed.jp>
2026-06-11 13:58:11 +0900

    Update ui.js

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,1 +1,22 @@
+function makeWindowDraggable(panel, header) {
+  let offsetX = 0;
+  let offsetY = 0;
+  let isDown = false;
+
+  header.addEventListener("mousedown", e => {
+    isDown = true;
+    offsetX = e.clientX - panel.offsetLeft;
+    offsetY = e.clientY - panel.offsetTop;
+  });
+
+  document.addEventListener("mousemove", e => {
+    if (!isDown) return;
+    panel.style.left = (e.clientX - offsetX) + "px";
+    panel.style.top = (e.clientY - offsetY) + "px";
+  });
+
+  document.addEventListener("mouseup", () => {
+    isDown = false;
+  });
+}
 
