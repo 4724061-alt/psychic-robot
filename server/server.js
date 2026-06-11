57db394a6d9feda3c753d8ea7b65a5bbd7d749e0
4724061-alt <4724061@gse.okayama-c.ed.jp>
2026-06-11 14:42:54 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*" }
+const io = new Server(server);
+
+server.listen(process.env.PORT || 3000);
+
 });
 
 // ▼ これが超重要（public フォルダを配信）
