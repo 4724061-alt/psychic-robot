1bb3ca99d3886bd0af827a26974e6f291b8516ef
4724061-alt <4724061@gse.okayama-c.ed.jp>
2026-06-11 13:44:09 +0900

    Update system.js

--- server/sockets/system.js
+++ server/sockets/system.js
@@ -1,1 +1,16 @@
+module.exports = (io, socket) => {
+  console.log("接続:", socket.id);
+
+  socket.on("joinUser", userId => {
+    socket.join("user:" + userId);
+  });
+
+  socket.on("joinCommunity", communityId => {
+    socket.join("community:" + communityId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("切断:", socket.id);
+  });
+};
 
